bb425a6fee2d9ff98bed388de2d90eeb3ba80bb6
david-morales-33 <davidmorales33.97@gmail.com>
2024-11-23 17:39:37 -0500

    Integracion de entidad <Role>

---
+++ administrative/src/main/java/com/dmx/role/domain/Role.java
@@ -0,0 +1,52 @@
+package com.dmx.role.domain;
+
+import com.dmx.shared.domain.RoleId;
+
+public final class Role {
+    private final RoleId id;
+    private final RoleName name;
+    private final RoleDescription description;
+
+    public Role(RoleId id, RoleName name, RoleDescription description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Role fromPrimitives(RoleDTO data) {
+        return new Role(
+                new RoleId(data.id()),
+                new RoleName(data.name()),
+                new RoleDescription(data.description())
+        );
+    }
+
+    public RoleDTO toPrimitives() {
+        return new RoleDTO(
+                this.id.value(),
+                this.name.value(),
+                this.description.value()
+        );
+    }
+
+    public RoleId getId() {
+        return this.id;
+    }
+
+    public RoleName getName() {
+        return this.name;
+    }
+
+    public RoleDescription getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + this.id +
+                ", name=" + this.name +
+                ", description=" + this.description +
+                '}';
+    }
+}

4b32833c0ed381dd5f3b2eaf170fbaf03315bb3b
david-morales-33 <davidmorales33.97@gmail.com>
2024-11-27 22:07:19 -0500

    Integracion de excepciones en entidades <Bus>

---
+++ shared/src/main/java/com/dmx/shared/domain/bus/query/QueryHandlerExecutionException.java
@@ -0,0 +1,7 @@
+package com.dmx.shared.domain.bus.query;
+
+public final class QueryHandlerExecutionException extends RuntimeException {
+    public QueryHandlerExecutionException(Throwable message) {
+        super(message);
+    }
+}

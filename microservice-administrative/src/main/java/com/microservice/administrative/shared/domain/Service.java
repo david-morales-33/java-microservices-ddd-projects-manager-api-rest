fc6c8a2815fc49d2f090a9f54e3e5407390aea0f
david-morales-33 <davidmorales33.97@gmail.com>
2025-03-07 23:31:06 -0500

    ADD: anotation <Service>

---
+++ microservice-administrative/src/main/java/com/microservice/administrative/shared/domain/Service.java
@@ -0,0 +1,9 @@
+package com.microservice.administrative.shared.domain;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Inherited
+public @interface Service {
+}

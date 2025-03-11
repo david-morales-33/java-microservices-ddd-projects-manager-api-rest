cfe2581faca750e0a2ac72a57638e47fe120ebda
david-morales-33 <davidmorales33.97@gmail.com>
2025-03-11 12:44:07 -0500

    ADD: project entities test, service: <Administrative-service>

---
+++ microservice-administrative/src/test/java/com/microservice/administrative/project/domain/ProjectTeamCounterTest.java
@@ -0,0 +1,4 @@
+package com.microservice.administrative.project.domain;
+
+public class ProjectTeamCounterTest {
+}
---
+++ microservice-administrative/src/test/java/com/microservice/administrative/shared/domain/FloatTest.java
@@ -0,0 +1,4 @@
+package com.microservice.administrative.shared.domain;
+
+public class FloatTest {
+}

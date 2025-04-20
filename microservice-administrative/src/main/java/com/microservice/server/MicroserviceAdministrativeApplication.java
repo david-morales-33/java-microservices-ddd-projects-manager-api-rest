a0b9df9fc98976ed5bd22cbf7576af90d8256e8d
david-morales-33 <davidmorales33.97@gmail.com>
2025-04-19 19:28:30 -0500

    REF: spring application config <exclude=HibernateJpaAutoConfiguration.class>; service: All

--- microservice-administrative/src/main/java/com/microservice/server/MicroserviceAdministrativeApplication.java
+++ microservice-administrative/src/main/java/com/microservice/server/MicroserviceAdministrativeApplication.java
@@ -2,10 +2,11 @@ package com.microservice.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 public class MicroserviceAdministrativeApplication {
 
 	public static void main(String[] args) {
--- microservice-media/src/main/java/com/microservice/server/MicroserviceMediaApplication.java
+++ microservice-media/src/main/java/com/microservice/server/MicroserviceMediaApplication.java
@@ -2,14 +2,14 @@ package com.microservice.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 public class MicroserviceMediaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceMediaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceMediaApplication.class, args);
+    }
 
 }
